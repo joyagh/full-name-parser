989c2a45cc7223ed5dc92de252b57d1ce4572da3
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-13 13:05:21 -0400

    added scanner input

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(" Below enter your name in the following formats: First Last or First Middle Last");
+        System.out.print("Full Name: ");
+        String fullName = input.next();
+
+
+
+    }
 }
